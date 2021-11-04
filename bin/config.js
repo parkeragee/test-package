0bea26881a64907895baa365cc5ff017581739fc
Parker Agee <parker@parkeragee.com>
2021-11-04 12:42:20 -0500

    Update config.js

--- bin/config.js
+++ bin/config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const shell = require("shelljs");
 const prettierConfig = require('./prettier.config');
 const stylelintConfig = require('./stylelint');
-const prettierIgnoreConfig = require('./prettierignore');
+// const prettierIgnoreConfig = require('./prettierignore');
 
 process.env.PATH += (path.delimiter + path.join(process.cwd(), 'node_modules', '.bin'));
 
@@ -15,5 +15,5 @@ console.log(process.env.PATH);
 shell.echo("Setting up your StyleLint config file..");
 shell.exec(`echo \'${JSON.stringify(stylelintConfig, null, 2)}\' > .stylelintrc`);
 
-shell.echo("Setting up your .prettierignore config file..");
-shell.exec(`echo \'${prettierIgnoreConfig}\' > .prettierignore`);
+// shell.echo("Setting up your .prettierignore config file..");
+// shell.exec(`echo \'${prettierIgnoreConfig}\' > .prettierignore`);
