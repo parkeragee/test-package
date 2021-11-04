9cfef4deea483cb7408ef38fa5af7b28427c3efe
Parker Agee <parker@parkeragee.com>
2021-11-04 12:23:12 -0500

    Update prettier.js

--- bin/prettier.js
+++ bin/prettier.js
@@ -3,8 +3,5 @@ const shell = require("shelljs");
 const path = require("path");
 const prettierConfig = require('./prettier-config');
 
-// process.env.PATH += (path.delimiter + path.join(process.cwd(), 'node_modules', '.bin'));
-// shell.exec("prettier 'src/**/*.{js,json}' --write");
-
 shell.echo("Setting up your Prettier config files now..");
 shell.exec(`echo \'${JSON.stringify(prettierConfig, null, 2)}\' > .prettier.config.js`);
