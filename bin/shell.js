6f2155e95cf67717b0b1d0b706bbcdf5aaac6300
Parker Agee <parker@parkeragee.com>
2021-11-04 12:10:10 -0500

    Update shell.js

--- bin/shell.js
+++ bin/shell.js
@@ -5,4 +5,5 @@ const path = require("path");
 // process.env.PATH += (path.delimiter + path.join(process.cwd(), 'node_modules', '.bin'));
 // shell.exec("prettier 'src/**/*.{js,json}' --write");
 
-shell.echo("Hey now");
+shell.echo("Setting up your config files now..");
+shell.exec('echo \'14\' > .nvmrc');
