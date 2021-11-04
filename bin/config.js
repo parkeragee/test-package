98fbb95c1d24211c51d327605c5b9e95c58c191e
Parker Agee <parker@parkeragee.com>
2021-11-04 12:33:54 -0500

    Update config.js

--- bin/config.js
+++ bin/config.js
@@ -4,7 +4,7 @@ const prettierConfig = require('./prettier.config');
 const stylelintConfig = require('./stylelint');
 
 shell.echo("Setting up your Prettier config files now..");
-shell.cp(prettierConfig, './');
+shell.cp('-R', prettierConfig, './');
 
 shell.echo("Setting up your StyleLint config file..");
 shell.exec(`echo \'${JSON.stringify(stylelintConfig, null, 2)}\' > .stylelintrc`);
