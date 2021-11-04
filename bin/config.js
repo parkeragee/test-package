2f749a82742262add10210bdc844541e4c1bb1c2
Parker Agee <parker@parkeragee.com>
2021-11-04 12:52:55 -0500

    aa

--- bin/config.js
+++ bin/config.js
@@ -1,17 +1,13 @@
 #! /usr/bin/env node
-const path = require('path');
 const shell = require("shelljs");
-const prettierConfig = require('./prettier.config');
-const stylelintConfig = require('./stylelint');
-// const prettierIgnoreConfig = require('./prettierignore');
 
-const filePath = process.env.PATH += (path.delimiter + path.join(process.cwd(), 'node_modules', 'test-package'));
+const filesToCopy = [
+    '.stylelintrc',
+    'prettierignore',
+    'prettier.config.js'
+];
 
-// shell.echo("Setting up your Prettier config files now..");
-// shell.exec(`echo \'${JSON.stringify(stylelintConfig, null, 2)}\' > .stylelintrc`);
-
-shell.echo("Setting up your StyleLint config file..");
-shell.exec(`echo \'${JSON.stringify(stylelintConfig, null, 2)}\' > .stylelintrc`);
-
-shell.echo("Setting up your .prettierignore config file..");
-shell.cp('-R', `${__dirname}/prettierignore`, './prettierignore');
+for (const file of filesToCopy) {
+    shell.echo(`Setting up your ${file} file..`);
+    shell.cp('-R', `${__dirname}/${file}`, `./${file}`);
+}
