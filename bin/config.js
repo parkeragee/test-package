5b254bcc6119fc90607dd0dba55791cda2538886
Parker Agee <parker@parkeragee.com>
2021-11-04 12:38:46 -0500

    a

--- bin/config.js
+++ bin/config.js
@@ -2,9 +2,13 @@
 const shell = require("shelljs");
 const prettierConfig = require('./prettier.config');
 const stylelintConfig = require('./stylelint');
+const prettierIgnoreConfig = require('./prettierignore');
 
-shell.echo("Setting up your Prettier config files now..");
-shell.cp('-R', prettierConfig, './');
+// shell.echo("Setting up your Prettier config files now..");
+// shell.exec(`echo \'${JSON.stringify(stylelintConfig, null, 2)}\' > .stylelintrc`);
 
 shell.echo("Setting up your StyleLint config file..");
 shell.exec(`echo \'${JSON.stringify(stylelintConfig, null, 2)}\' > .stylelintrc`);
+
+shell.echo("Setting up your .prettierignore config file..");
+shell.exec(`echo \'${prettierIgnoreConfig}\' > .prettierignore`);
