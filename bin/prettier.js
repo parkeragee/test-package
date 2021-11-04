9b39ca6ac0eed788d74410d10abb366de8afec67
Parker Agee <parker@parkeragee.com>
2021-11-04 12:14:41 -0500

    prettier

--- bin/prettier.js
+++ bin/prettier.js
@@ -1,9 +1,10 @@
 #! /usr/bin/env node
 const shell = require("shelljs");
 const path = require("path");
+const prettierConfig = require('./prettier-config');
 
 // process.env.PATH += (path.delimiter + path.join(process.cwd(), 'node_modules', '.bin'));
 // shell.exec("prettier 'src/**/*.{js,json}' --write");
 
-shell.echo("Setting up your config files now..");
-shell.exec('echo \'14\' > .nvmrc');
+shell.echo("Setting up your Prettier config files now..");
+shell.exec(`echo \'${prettierConfig}\' > .prettier.config.js`);
