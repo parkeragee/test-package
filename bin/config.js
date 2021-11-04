0b96763e7e029572d27dc02766af82f52fa5a9c1
Parker Agee <parker@parkeragee.com>
2021-11-04 12:30:53 -0500

    test

---
+++ bin/config.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+const shell = require("shelljs");
+const prettierConfig = require('./prettier.config');
+const stylelintConfig = require('./stylelint');
+
+shell.echo("Setting up your Prettier config files now..");
+shell.cp(prettierConfig, './');
+
+shell.echo("Setting up your StyleLint config file..");
+shell.exec(`echo \'${JSON.stringify(stylelintConfig, null, 2)}\' > .stylelintrc`);
--- bin/prettier.js
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env node
-const shell = require("shelljs");
-const path = require("path");
-const prettierConfig = require('./prettier-config');
-
-shell.echo("Setting up your Prettier config files now..");
-shell.exec(`echo \'${JSON.stringify(prettierConfig, null, 2)}\' > .prettier.config.js`);
